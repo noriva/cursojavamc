42ceea37915497c3dcab9cc4e860355710202139
noriva <jrmarson@bol.com.br>
2019-09-05 00:36:54 -0300

    Projeto foi criado

---
+++ src/main/java/com/marson/cursojavamc/CursojavamcApplication.java
@@ -0,0 +1,13 @@
+package com.marson.cursojavamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavamcApplication.class, args);
+	}
+
+}
