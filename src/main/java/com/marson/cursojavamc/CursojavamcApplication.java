387e2d9279b51200cfd53f140dbced116a94e777
noriva <jrmarson@bol.com.br>
2019-09-07 16:55:19 -0300

    Criando operacao de instanciacao

--- src/main/java/com/marson/cursojavamc/CursojavamcApplication.java
+++ src/main/java/com/marson/cursojavamc/CursojavamcApplication.java
@@ -1,13 +1,39 @@
 package com.marson.cursojavamc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursojavamcApplication {
+import com.marson.cursojavamc.domain.Categoria;
+import com.marson.cursojavamc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursojavamcApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository ;  
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojavamcApplication.class, args);
+		
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null , "Informática");
+		Categoria cat2 = new Categoria(null , "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
