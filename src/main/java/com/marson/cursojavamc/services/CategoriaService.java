891d392898ae576a6a0236ac57848fc893509d20
noriva <jrmarson@bol.com.br>
2019-09-27 21:54:31 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/marson/cursojavamc/services/CategoriaService.java
+++ src/main/java/com/marson/cursojavamc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marson.cursojavamc.domain.Categoria;
@@ -50,6 +53,12 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 	
-	
+	public Page<Categoria> findPage(Integer page , Integer linesPage , String orderBy , String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPage , Direction.valueOf(direction),orderBy);
+		
+		
+		return repo.findAll(pageRequest);
+		
+	}
 	
 }
